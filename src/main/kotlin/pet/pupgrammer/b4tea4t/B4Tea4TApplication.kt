46f8e5b68eeb20847389d2183df63a844b13f88d
NovaFox161 <nova@maday.dev>
2024-09-21 22:44:07 -0500

    Missing component annotation

--- src/main/kotlin/pet/pupgrammer/b4tea4t/B4Tea4TApplication.kt
+++ src/main/kotlin/pet/pupgrammer/b4tea4t/B4Tea4TApplication.kt
@@ -2,8 +2,10 @@ package pet.pupgrammer.b4tea4t
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 import pet.pupgrammer.b4tea4t.config.Config
 
+@Component
 @SpringBootApplication
 class B4Tea4TApplication
 
