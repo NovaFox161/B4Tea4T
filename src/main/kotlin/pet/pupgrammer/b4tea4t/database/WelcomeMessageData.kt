dbaa394f140c51f3a81d3a8bf8dad2d5e941b4bf
NovaFox161 <nova@maday.dev>
2025-01-29 19:53:04 -0600

    Fix missing table name and silence some noisy debug logs

--- src/main/kotlin/pet/pupgrammer/b4tea4t/database/WelcomeMessageData.kt
+++ src/main/kotlin/pet/pupgrammer/b4tea4t/database/WelcomeMessageData.kt
@@ -1,5 +1,8 @@
 package pet.pupgrammer.b4tea4t.database
 
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("welcome_messages")
 data class WelcomeMessageData(
     val guildId: Long,
     val enabled: Boolean,
